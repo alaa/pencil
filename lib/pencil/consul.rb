b63faf66aad6275eda080a110c1e1765f0d11d95
Alaa Qutaish <alaa.qutaish@gmail.com>
2015-05-03 18:13:40 +0200

    resync local containers with remote containers

--- lib/pencil/consul.rb
+++ lib/pencil/consul.rb
@@ -8,7 +8,31 @@ module Pencil
       @port = port
     end
 
+    def get_ids(local_containers)
+      local_containers.each_with_object([]) do |(k, v), acc|
+        acc << "#{v['image']}:#{k}"
+      end
+    end
+
+    def resync(local_containers)
+      ids = get_ids(local_containers)
+      registered_containers = get_registered_containers
+
+      zombies = registered_containers - ids
+      zombies.each do |zombie|
+        puts "deregistering #{zombie}" if deregister_service(zombie)
+      end
+
+    end
+
+    def deregister_service(id)
+      uri = "http://#{host}:#{port}/v1/agent/service/deregister/#{id}"
+      RestClient.get uri
+    end
+
     def register_service(service_id:, service_name:, host_port:)
+      puts "== syncing #{service_id}"
+
       uri = "http://#{host}:#{port}/v1/agent/service/register"
       body = {
         "ID" => "#{service_name}:#{service_id}",
@@ -20,8 +44,17 @@ module Pencil
         }
       }.to_json
 
-      puts body
       RestClient.put uri, body, :content_type => :json, :accept => :json
+    end
+
+    def get_registered_containers
+      uri = "http://#{host}:#{port}/v1/agent/services"
+      resp = RestClient.get uri
+
+      services = JSON.parse(resp)
+      services.each_with_object([]) do |service, acc|
+        acc << service.first
+      end
     end
 
     private
