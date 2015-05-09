5c4806505b76fcc21e84cfc9fe2b69bd6695efe6
Alaa Qutaish <alaa.qutaish@gmail.com>
2015-05-09 02:34:29 +0200

    refactoring consul

--- lib/pencil/consul.rb
+++ lib/pencil/consul.rb
@@ -2,91 +2,113 @@ require 'rest-client'
 require 'logger'
 
 module Pencil
-  class Consul
 
+  class Consul
     def initialize(host:, port:)
-      @host = host.nil? ? `hostname` : host
+      @host = host
       @port = port
-      @consul_uri = "http://#{host}:#{port}"
       @logger = Logger.new(STDOUT)
     end
 
-    def resync(containers)
-      containers_ids = containers.keys
-      @logger.info("local container: #{containers_ids}")
-      registered_containers = get_registered_containers
-
-      stale_containers = registered_containers - containers_ids
-      @logger.info("stale containers: #{stale_containers}")
-      deregister_containers(stale_containers)
-
-      new_containers = containers_ids - registered_containers
-      @logger.info("new containers: #{new_containers}")
-      register_containers(containers, new_containers)
+    def resync(local_services)
+      services = Services.new(local_services.keys, get_registered_services)
+      deregister_services(services.to_deregister)
+      register_services(local_services, services.to_register)
     end
 
     private
-    attr_accessor :host, :port, :consul_uri
+    attr_accessor :host, :port
 
-    def consul_image_name(name, port)
-      name.split('/').last.split(':').first + '-' + port
-    end
+    def register_services(services, new_services)
+      services_data = services.select do |key, value|
+        new_services.include? key
+      end
 
-    def register_containers(containers, new_containers)
-      containers.each_pair do |key, value|
-        if new_containers.include?(key)
-          register_container(container_id: key,
-                             image_name: value['image'],
-                             host_port: value['host_port'],
-                             service_port: value['service_port'])
-        end
+      services_data.each do |id, options|
+        register_service(service_id: id, options: options)
       end
     end
 
-    def register_container(container_id:, image_name:, host_port:, service_port:)
-      puts "+++ " + container_id
-      uri = consul_uri + "/v1/agent/service/register"
-
+    def register_service(service_id:, options:)
       body = {
-        "ID" => container_id,
-        "Name" => consul_image_name(image_name, service_port),
-        "Port" => host_port.to_i,
+        "ID" => service_id,
+        "Name" => consul_image_name(options['image'], options['service_port']),
+        "Port" => options['host_port'].to_i,
         "Check" => {
-          "Script" => "curl -Ss http://#{host}:#{host_port}",
+          "Script" => "curl -Ss http://#{host}:#{options['host_port']}",
           "Interval" => "10s",
         }
       }.to_json
 
-      resp = RestClient.put uri, body, :content_type => :json, :accept => :json
-      @logger.info("Consul response: #{resp}")
+      api_client(method: :put, uri: consul_register_service_uri, body: body)
+      @logger.info "registering: #{service_id}"
     end
 
-    def get_registered_containers
-      uri = consul_uri + "/v1/agent/services"
-      resp = RestClient.get uri
-
-      services = JSON.parse(resp)
+    def get_registered_services
+      services = api_client(method: :get, uri: consul_services_uri)
       services.each_with_object([]) do |service, acc|
         acc << service.first
       end
     end
 
-    def get_containers_ids(local_containers)
-      local_containers.each_with_object([]) do |(id, options), ids|
-        ids << id
+    def get_services_ids(local_services)
+      local_services.each_with_object([]) do |(id, options), acc|
+        acc << id
       end
     end
 
-    def deregister_containers(ids)
+    def deregister_services(ids)
       ids.each do |id|
-        deregister_container(id)
+        deregister_service(id)
       end
     end
 
-    def deregister_container(id)
-      uri = consul_uri + "/v1/agent/service/deregister/#{id}"
-      puts "--- #{id}"
-      RestClient.get uri
+    def deregister_service(id)
+      uri = consul_deregister_service_uri(id)
+      api_client(method: :get, uri: uri)
+      @logger.info "deregistering: #{id}"
+    end
+
+    def api_client(method:, uri:, body: nil)
+      case method
+      when :get
+        resp = RestClient.get(uri)
+        JSON.parse(resp) if resp.size > 2
+      when :put
+        resp = RestClient.put(uri, body, content_type: :json, accept: :json)
+      else
+        fail 'consul exception'
+      end
+    end
+
+    def consul_image_name(name, port)
+      name.split('/').last.split(':').first + '-' + port
+    end
+
+    def consul_url
+      "http://#{host}:#{port}"
+    end
+
+    def consul_register_service_uri
+      consul_url + "/v1/agent/service/register"
+    end
+
+    def consul_deregister_service_uri(service_id)
+      consul_url + "/v1/agent/service/deregister/#{service_id}"
+    end
+
+    def consul_services_uri
+      consul_url + "/v1/agent/services"
+    end
+  end
+
+  Services = Struct.new(:local_services, :remote_services) do
+    def to_register
+      local_services - remote_services
+    end
+
+    def to_deregister
+      remote_services - local_services
     end
   end
 end
