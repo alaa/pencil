deb9d4393c78a086266f0bf77ed45f377d8a367d
Alaa Qutaish <alaa.qutaish@gmail.com>
2015-08-22 23:55:30 +0200

    move docker related code to docker module

--- lib/pencil/consul.rb
+++ lib/pencil/consul.rb
@@ -30,59 +30,23 @@ module Pencil
     end
 
     def register_service(service_id:, options:)
-      service_name = consul_image_name(options['image'], options['service_port'])
-
-      service_check = health_check(options['env'], options['host_port'])
-
       resource = endpoints.register_service(service_id: service_id,
-                                            service_name: service_name,
+                                            service_name: options['name'],
                                             host_port: options['host_port'],
-                                            health_check: service_check,
+                                            health_check: options['check'],
                                             health_check_interval: 5,
-                                            tags: pencil_env(options['env']))
+                                            tags: options['tags'])
 
-      API::HTTP.request(resource: resource)
+      API::HTTP.request(body: resource)
       @logger.info "registering: #{service_id}"
     end
 
     def get_registered_services
       resource = endpoints.services
-      services = API::HTTP.request(resource: resource)
+      services = API::HTTP.request(body: resource)
       services.each_with_object([]) do |service, acc|
         acc << service.first
       end
-    end
-
-    # Extract pencil related variables starting with SRV_
-    def pencil_env(env)
-      vars = []
-      env.each do |e|
-        e =~ /^SRV_/i ? vars << e : ""
-      end
-      vars
-    end
-
-    # Extract health check script from env, if not passed fall back to the
-    # default health check
-    def health_check(env, port)
-      hash = Hash.new
-      vars = pencil_env(env)
-
-      vars.each do |var|
-        key, value = var.split('=')
-        hash[key] = value
-      end
-
-      if hash.has_key?('SRV_HEALTH_CHECK')
-        return check_formatter(hash['SRV_HEALTH_CHECK'], host, port)
-      end
-
-      check_formatter("curl -Ss %<host>s:%<port>s", host, port)
-    end
-
-    # Replace Check variables references with their corresponding values
-    def check_formatter(script, host, port)
-      sprintf(script, {host: host, port: port})
     end
 
     def deregister_services(ids)
@@ -93,7 +57,7 @@ module Pencil
 
     def deregister_service(id)
       resource = endpoints.deregister_service(id)
-      API::HTTP.request(resource: resource)
+      API::HTTP.request(body: resource)
       @logger.info "deregistering: #{id}"
     end
 
@@ -101,10 +65,6 @@ module Pencil
       local_services.each_with_object([]) do |(id, options), acc|
         acc << id
       end
-    end
-
-    def consul_image_name(name, port)
-      name.split('/').last.split(':').first + '-' + port
     end
 
     Services = Struct.new(:local_services, :remote_services) do
--- lib/pencil/docker.rb
+++ lib/pencil/docker.rb
@@ -3,8 +3,8 @@ require 'docker'
 module Pencil
   class Docker
 
-    def initialize(hostname = 'localhost')
-      @hostname = hostname
+    def initialize(host:)
+      @host = host
     end
 
     def all
@@ -15,22 +15,27 @@ module Pencil
         image = container['Config']['Image']
         cid = container['Id']
         env = container['Config']['Env']
+        tags = extract_tags(env)
 
         ports.each do |service_port, host_port|
           unless host_port.nil?
+            s_port = service_port.split('/').first
+            h_port = host_port.first['HostPort']
+
             acc[cid] = {}
-            acc[cid]['service_port'] = service_port.split('/').first
-            acc[cid]['host_port'] = host_port.first['HostPort']
+            acc[cid]['service_port'] = s_port
+            acc[cid]['host_port'] = h_port
             acc[cid]["image"] = image
-            acc[cid]["env"] = env
+            acc[cid]["tags"] = tags
+            acc[cid]["check"] = check(tags, host, h_port)
+            acc[cid]["name"] = service_name(tags, image, s_port)
           end
         end
-
       end
     end
 
     private
-    attr_accessor :hostname
+    attr_accessor :host
 
     def list_running_containers
       ::Docker::Container.all
@@ -40,6 +45,46 @@ module Pencil
       containers.each_with_object([]) do |container, acc|
         acc << container.json
       end
+    end
+
+    def extract_tags(env_vars)
+      env_vars.each_with_object([]) do |var, tags|
+        var =~ /^SRV_/ ? tags << var : ""
+      end
+    end
+
+    # Construct Service Name
+    def service_name(tags, image, port)
+      tags.each do |tag|
+        if tag =~ /^SRV_NAME/
+          name = tag.split('=')[1]
+          return name
+        end
+      end
+      service_default_name(image, port)
+    end
+
+    def service_default_name(image, port)
+      image.split('/').last.split(':').first + '-' + port
+    end
+
+    # Construct Check script
+    def check(tags, host, port)
+      tags.each_with_object([]) do |tag, checks|
+        if tag =~ /^SRV_HEALTH_CHECK/
+          script = tag.split('=')[1]
+          return check_formatter(script, host, port)
+        end
+      end
+      check_formatter(default_health_check_script, host, port)
+    end
+
+    def check_formatter(script, host, port)
+      sprintf(script, {host: host, port: port})
+    end
+
+    def default_health_check_script
+      "curl -Ss http://%<host>s:%<port>s"
     end
   end
 end
