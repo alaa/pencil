fb54d428a0877080a887381778a748d9ccac8b3e
Alaa Qutaish <alaa.qutaish@gmail.com>
2015-08-27 12:41:07 +0200

    FIX: Add STDOUT.sync to logger to flush logs from docker to syslog

--- lib/pencil/logging.rb
+++ lib/pencil/logging.rb
@@ -8,9 +8,10 @@ module Pencil
 
     module ClassMethods
       def logger
+        STDOUT.sync = true
         logger ||= Logger.new(STDOUT)
         logger.formatter = proc do |severity, datetime, progname, msg|
-          "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} [#{severity}] #{msg}\n"
+          "[pencil] #{datetime.strftime('%Y-%m-%d %H:%M:%S')} [#{severity}] #{msg}\n"
         end
         logger
       end
